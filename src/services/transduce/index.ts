5ad58b16864785e7e05bf0e8a13bbf2ba452b04a
Joseph Zapantis <joseph@zapantis.us>
2021-12-06 10:49:17 -0500

    removed logs

--- src/services/transduce/index.ts
+++ src/services/transduce/index.ts
@@ -1,3 +1,33 @@
+const sampleArray = [
+    {
+        record: {
+            values: {
+                prop1: "val1",
+                prop2: "val2",
+                number1: 1,
+                number2: 2
+            }
+        }
+    },
+    {
+        record: {
+            values: {
+                prop3: "val3",
+                prop4: "val4",
+                number3: 3,
+                number4: 4
+            }
+        }
+    }
+]
+
 export function transduce() {
-    return true;
+    const mappedRecords = stevoMapper(sampleArray);
+    return mappedRecords;
+}
+
+const stevoMapper = (arr: Array<any>) => {
+    arr.reduce((collection, curRecord) => {
+        return curRecord;
+    }, [])
 }
